099b4448811abc44725bd8982433e7ca8218de52
Lars Stoltenow <penma@penma.de>
2011-08-12 02:14:41 +0200

    add error output to imlib loading

--- single/single.c
+++ single/single.c
@@ -15,6 +15,25 @@ void spam(const char *foo) {
 	write(2, foo, strlen(foo));
 }
 
+const char *imliberr(Imlib_Load_Error e) {
+	switch (e) {
+	case IMLIB_LOAD_ERROR_FILE_DOES_NOT_EXIST                : return "No such file or directory";
+	case IMLIB_LOAD_ERROR_FILE_IS_DIRECTORY                  : return "Is a directory";
+	case IMLIB_LOAD_ERROR_PERMISSION_DENIED_TO_READ          : return "Permission denied";
+	case IMLIB_LOAD_ERROR_UNKNOWN                            :
+	case IMLIB_LOAD_ERROR_NO_LOADER_FOR_FILE_FORMAT          : return "No Imlib2 loader for that file format";
+	case IMLIB_LOAD_ERROR_PATH_TOO_LONG                      : return "Filename too long";
+	case IMLIB_LOAD_ERROR_PATH_COMPONENT_NON_EXISTANT        : return "No such file or directory (along path)";
+	case IMLIB_LOAD_ERROR_PATH_COMPONENT_NOT_DIRECTORY       : return "Not a directory";
+	case IMLIB_LOAD_ERROR_PATH_POINTS_OUTSIDE_ADDRESS_SPACE  : return "Bad address";
+	case IMLIB_LOAD_ERROR_TOO_MANY_SYMBOLIC_LINKS            : return "Too many levels of symbolic links";
+	case IMLIB_LOAD_ERROR_OUT_OF_MEMORY                      : return "Out of memory";
+	case IMLIB_LOAD_ERROR_OUT_OF_FILE_DESCRIPTORS            : return "Too many open files";
+	case IMLIB_LOAD_ERROR_PERMISSION_DENIED_TO_WRITE         : return "Permission denied (to write to directory)";
+	case IMLIB_LOAD_ERROR_OUT_OF_DISK_SPACE                  : return "No space left on device";
+	default                                                  : return "Unknown error";
+	}
+
 Imlib_Image *current_image = NULL;
 
 int load_image(char *filename) {
@@ -24,7 +43,15 @@ int load_image(char *filename) {
 	}
 
 	/* TODO error handling */
-	current_image = imlib_load_image(filename);
+	Imlib_Load_Error err;
+	current_image = imlib_load_image_with_error_return(filename, &err);
+
+	if (current_image == NULL) {
+		const char *errmsg = imliberr(err);
+		spam("imlib load error: ");
+		spam(errmsg);
+		spam("\n");
+	}
 
 	return 0;
 }
