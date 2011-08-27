190b11b9141e434cda3cd0491accff430351a44a
Lars Stoltenow <penma@penma.de>
2011-08-28 01:08:49 +0200

    common/types: new rect type
    still not sure... this must end in epic overdesign.

--- common/types.h
+++ common/types.h
@@ -6,8 +6,17 @@ struct coord {
 	union { int y, height,   vertical; };
 };
 
+struct rect {
+	struct coord topleft;
+	struct coord dimensions;
+};
+
 static inline struct coord COORD(int x, int y) {
 	return (struct coord){ .x = x, .y = y };
+}
+
+static inline struct rect RECT(struct coord tl, struct coord dim) {
+	return (struct rect){ .topleft = tl, .dimensions = dim };
 }
 
 #endif
