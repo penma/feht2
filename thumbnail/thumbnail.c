caa0a898bf67fc410efae23a426cbf123b21218d
Lars Stoltenow <penma@penma.de>
2011-08-31 15:59:00 +0200

    update onscreen thumbs before all others
    see also #1 -- it doesn't do all the priorities and the method will
    likely change together with filelist management.

--- thumbnail/thumbnail.c
+++ thumbnail/thumbnail.c
@@ -6,9 +6,11 @@
 
 #include "common/imlib_error.h"
 #include "thumbnail/thumbnail.h"
+#include "thumbnail/layout.h"
 
 int thumb_width = 200, thumb_height = 150;
 struct thumbnail **thumbnails = NULL;
+extern int scroll_offset, win_width, win_height;
 
 struct thumbnail *find_thumbnail_by_name(char *filename) {
 	struct thumbnail **p = thumbnails;
@@ -21,92 +23,154 @@ struct thumbnail *find_thumbnail_by_name(char *filename) {
 	return NULL;
 }
 
-int try_update_thumbnails() {
-	if (thumbnails == NULL) {
-		return 0;
+static struct thumbnail *next_to_update(struct thumbnail **list, struct layout *layout) {
+	if (list == NULL) {
+		return NULL;
 	}
 
-	struct thumbnail **p = thumbnails;
+	/* we first check all those that are in view. */
+
+	struct rect on_screen = RECT(
+		COORD(0, scroll_offset),
+		layout->window
+	);
+
+	struct thumbnail **p = list;
+	int frame_num = 0;
 
 	while (*p != NULL) {
-		if (!(*p)->failed && (
-		    (*p)->imlib == NULL ||
-		    (*p)->width != thumb_width ||
-		    (*p)->height != thumb_height)) {
-			/* update */
+		struct rect frame_rect = layout_frame_rect_by_number(layout, frame_num);
 
-			struct thumbnail *t = (*p);
-
-			fprintf(stderr, "trying to update thumbnail for %s\n", t->filename);
-
-			if (t->imlib != NULL) {
-				imlib_context_set_image(t->imlib);
-				imlib_free_image_and_decache();
-			}
-
-			Imlib_Load_Error err;
-			Imlib_Image orig = imlib_load_image_with_error_return(t->filename, &err);
-
-			if (orig == NULL) {
-				const char *errmsg = imlib_load_error_string(err);
-				fprintf(stderr, "error trying to load %s: %s\n", t->filename, errmsg);
-				t->failed = 1;
-
-				return 1;
-			}
-
-			imlib_context_set_image(orig);
-			int width  = imlib_image_get_width();
-			int height = imlib_image_get_height();
-
-			/* compute size and position of the preview inside the thumb frame */
-
-			int dw, dh, dx, dy;
-
-			dw = width < thumb_width ? width : thumb_width;
-			dh = dw * (double)height / width;
-
-			if (dh > thumb_height) {
-				dh = thumb_height;
-				dw = dh * (double)width / height;
-			}
-
-			dx = (thumb_width  - dw) / 2;
-			dy = (thumb_height - dh) / 2;
-
-			/* create the thumbnail */
-
-			t->imlib = imlib_create_image(thumb_width, thumb_height);
-			imlib_context_set_image(t->imlib);
-			imlib_image_set_has_alpha(1);
-
-			imlib_context_set_blend(0);
-			imlib_context_set_color(0, 0, 0, 0);
-			imlib_image_fill_rectangle(0, 0, thumb_width, thumb_height);
-			imlib_context_set_blend(1);
-
-			imlib_blend_image_onto_image(
-				orig, 1,
-				0, 0, width, height,
-				dx, dy, dw, dh
-			);
-
-			t->width  = thumb_width;
-			t->height = thumb_height;
-
-			/* can free original image now */
-
-			imlib_context_set_image(orig);
-			imlib_free_image();
-
-			/* update no more than one */
-
-			return 1;
+		if (
+			rect_intersect(frame_rect, on_screen) &&
+			!(*p)->failed                         &&
+			(
+				(*p)->imlib  == NULL          ||
+				(*p)->width  != thumb_width   ||
+				(*p)->height != thumb_height
+			)
+		) {
+			return (*p);
 		}
 
 		p++;
+		frame_num++;
 	}
 
-	return 0;
+	/* then all the others */
+
+	p = list;
+	frame_num = 0;
+
+	while (*p != NULL) {
+		if (
+			!(*p)->failed                         &&
+			(
+				(*p)->imlib  == NULL          ||
+				(*p)->width  != thumb_width   ||
+				(*p)->height != thumb_height
+			)
+		) {
+			return (*p);
+		}
+
+		p++;
+		frame_num++;
+	}
+
+	/* none! */
+
+	return NULL;
+}
+
+int try_update_thumbnails() {
+	/* XXX construct layout elsewhere */
+	struct layout *L = layout_new();
+	L->window  = COORD(win_width, win_height);
+	L->spacing = COORD(20, 20);
+	L->frame   = COORD(thumb_width, thumb_height + 12); /* text height, XXX */
+
+	{
+		L->frame_count = 0;
+		struct thumbnail **p = thumbnails;
+		while (*p != NULL) {
+			p++;
+			L->frame_count++;
+		}
+	}
+
+	layout_recompute(L);
+
+	struct thumbnail *t = next_to_update(thumbnails, L);
+
+	if (t == NULL) {
+		return 0;
+	}
+
+	fprintf(stderr, "trying to update thumbnail for %s\n", t->filename);
+
+	if (t->imlib != NULL) {
+		imlib_context_set_image(t->imlib);
+		imlib_free_image_and_decache();
+	}
+
+	Imlib_Load_Error err;
+	Imlib_Image orig = imlib_load_image_with_error_return(t->filename, &err);
+
+	if (orig == NULL) {
+		const char *errmsg = imlib_load_error_string(err);
+		fprintf(stderr, "error trying to load %s: %s\n", t->filename, errmsg);
+		t->failed = 1;
+
+		return 1;
+	}
+
+	imlib_context_set_image(orig);
+	int width  = imlib_image_get_width();
+	int height = imlib_image_get_height();
+
+	/* compute size and position of the preview inside the thumb frame */
+
+	int dw, dh, dx, dy;
+
+	dw = width < thumb_width ? width : thumb_width;
+	dh = dw * (double)height / width;
+
+	if (dh > thumb_height) {
+		dh = thumb_height;
+		dw = dh * (double)width / height;
+	}
+
+	dx = (thumb_width  - dw) / 2;
+	dy = (thumb_height - dh) / 2;
+
+	/* create the thumbnail */
+
+	t->imlib = imlib_create_image(thumb_width, thumb_height);
+	imlib_context_set_image(t->imlib);
+	imlib_image_set_has_alpha(1);
+
+	imlib_context_set_blend(0);
+	imlib_context_set_color(0, 0, 0, 0);
+	imlib_image_fill_rectangle(0, 0, thumb_width, thumb_height);
+	imlib_context_set_blend(1);
+
+	imlib_blend_image_onto_image(
+		orig, 1,
+		0, 0, width, height,
+		dx, dy, dw, dh
+	);
+
+	t->width  = thumb_width;
+	t->height = thumb_height;
+
+	/* can free original image now */
+
+	imlib_context_set_image(orig);
+	imlib_free_image();
+
+	/* update no more than one */
+
+	return 1;
 }
 
